7a96a8947c90ac787db37460e0a4f3ee6dacfaba
dongdongsw <fishman4535@gmail.com>
2025-08-08 15:28:21 +0900

    <서동현> 일단 게시판 상세내용만 적용해서 올림, 따로 설치할 필요 없음, 그저 패치하고 풀 하시면 됩니다

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Spinner}width="500" alt="profile" />
-        <p>
-          Hello mother Fucker
-
-
-        </p>
-        
-        
-        
-
-      </header>
+      
     </div>
   );
 }
