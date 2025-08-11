74a5b13c2227f753a098f05972cf259f7028e179
dongdongsw <fishman4535@gmail.com>
2025-08-11 16:00:37 +0900

    <서동현> 디자인 템플릿 전부다 푸쉬

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import Spinner from './1234.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
-=======
-      <header className="App-header">
-        <img src={Spinner}width="500" alt="profile" />
-        <p>
-          
-          Hello mother Fucker
 
-
-        </p>
-        
-        
-        
-
-      </header>
->>>>>>> 283308f9a83ecdfa24acc45efa1807beb4557cf7
     </div>
   );
 }
 
 export default App;
+ 
